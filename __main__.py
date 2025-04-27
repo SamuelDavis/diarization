ddf3b91d31aab0e97da610a9425cbb34fb4d8726
Samuel Davis <samueljakdavis@gmail.com>
2025-04-27 19:17:14 -0400

    omit unused code

--- __main__.py
+++ __main__.py
@@ -1,13 +1,12 @@
-import warnings
 from os import environ
 from sys import argv
 
 import torch
-from pyannote.audio.utils.reproducibility import ReproducibilityWarning
 
 from utils import align, annotate, parse_args, preprocess, transcribe
 
 assert torch.cuda.is_available()
+assert torch.version.cuda == "12.8"
 token = environ.get("HUGGING_FACE_AUTH_TOKEN")
 assert token
 
